1269572b5b50e56269d2c1c59eba2f470c0ba592
Omkar Jagtap <152702656+meomkarjagtap@users.noreply.github.com>
2026-04-29 20:24:17 +0530

    Update blog.routes.js

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,33 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-let blogs = []; // temporary in-memory storage
+const {
+  createBlog,
+  getBlogs
+} = require("../controllers/blog.controller");
 
-// Create blog
-router.post("/", (req, res) => {
-  const { title, content, tags } = req.body;
-
-  const newBlog = {
-    id: Date.now(),
-    title,
-    content,
-    tags
-  };
-
-  blogs.push(newBlog);
-
-  console.log(JSON.stringify({
-    level: "info",
-    message: "Blog created",
-    blogId: newBlog.id
-  }));
-
-  res.json(newBlog);
-});
-
-// Get all blogs
-router.get("/", (req, res) => {
-  res.json(blogs);
-});
+router.post("/", createBlog);
+router.get("/", getBlogs);
 
 module.exports = router;
